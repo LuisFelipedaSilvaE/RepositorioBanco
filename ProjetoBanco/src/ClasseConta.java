cd10f450229d299d382b0bf81db652b2d264ee9f
lab04 <lab04@lab4-06>
2024-04-08 22:42:43 -0300

    Feito os métodos

--- ProjetoBanco/src/ClasseConta.java
+++ ProjetoBanco/src/ClasseConta.java
@@ -4,5 +4,25 @@ public class ClasseConta {
 	float atributoSaldoConta; // 0.0
 	String atributoTipoConta; // null
 	
-
+	ClassePessoa atributoPessoa;
+	
+	void metodoDepositar(float parametroValorEnviado) {
+		this.atributoSaldoConta = this.atributoSaldoConta + parametroValorEnviado;
+		// this.atributoSaldoConta += parametroValorEnviado;
+	}
+	
+	void metodoScar(float parametroValorSaque) {
+		
+		if (this.atributoSaldoConta < parametroValorSaque) {
+			System.out.println("Limite insuficiente! Pobre!");
+		}
+		else {
+			this.atributoSaldoConta = this.atributoSaldoConta - parametroValorSaque;
+		}
+	}
+	
+	void metodoTransferir(float parametroValorTransferido, ClasseConta origem, ClasseConta destino) {
+		origem.atributoSaldoConta -= parametroValorTransferido;
+		destino.atributoSaldoConta += parametroValorTransferido;
+	}
 }
--- ProjetoBanco/src/ClassePrincipal.java
+++ ProjetoBanco/src/ClassePrincipal.java
@@ -11,15 +11,11 @@ public class ClassePrincipal {
 		System.out.println(objeto_conta_um.atributoNumeroConta);
 		System.out.println(objeto_conta_um.atributoSaldoConta);
 		System.out.println(objeto_conta_um.atributoTipoConta);
-		System.out.println(objeto_conta_um.atributoNomePessoa);
-		System.out.println(objeto_conta_um.atributoDocumentoPessoa);
-		
+
 		System.out.println(objeto_conta_dois.atributoNumeroConta);
 		System.out.println(objeto_conta_dois.atributoSaldoConta);
 		System.out.println(objeto_conta_dois.atributoTipoConta);
-		System.out.println(objeto_conta_dois.atributoNomePessoa);
-		System.out.println(objeto_conta_dois.atributoDocumentoPessoa);
-		
+
 		// Adicionado dados das contas
 		objeto_conta_um.atributoNumeroConta = 123;
 		objeto_conta_um.atributoSaldoConta = 2_600.00F;
@@ -34,14 +30,34 @@ public class ClassePrincipal {
 		System.out.println(objeto_conta_um.atributoNumeroConta);
 		System.out.println(objeto_conta_um.atributoSaldoConta);
 		System.out.println(objeto_conta_um.atributoTipoConta);
-		System.out.println(objeto_conta_um.atributoNomePessoa);
-		System.out.println(objeto_conta_um.atributoDocumentoPessoa);
 		
 		System.out.println(objeto_conta_dois.atributoNumeroConta);
 		System.out.println(objeto_conta_dois.atributoSaldoConta);
 		System.out.println(objeto_conta_dois.atributoTipoConta);
-		System.out.println(objeto_conta_dois.atributoNomePessoa);
-		System.out.println(objeto_conta_dois.atributoDocumentoPessoa);
+		
+		objeto_conta_um.atributoPessoa = new ClassePessoa(); // Inst
+		
+		objeto_conta_um.atributoPessoa.atributoNomePessoa = "Fulano";
+		objeto_conta_um.atributoPessoa.atributoDocumentoPessoa = 333;
+		
+		objeto_conta_dois.atributoPessoa = new ClassePessoa(); // Inst
+		
+		objeto_conta_dois.atributoPessoa.atributoNomePessoa = "Ciclano";
+		objeto_conta_dois.atributoPessoa.atributoDocumentoPessoa = 444;
+		
+		objeto_conta_um.metodoDepositar(500.00F);
+		System.out.println(objeto_conta_um.atributoSaldoConta);
+		
+		objeto_conta_um.metodoDepositar(500.00F);
+		System.out.println(objeto_conta_um.atributoSaldoConta);
+		
+		objeto_conta_um.metodoScar(4_000.00F);
+		System.out.println(objeto_conta_um.atributoSaldoConta);
+		
+		objeto_conta_um.metodoTransferir(500.00F, objeto_conta_um, objeto_conta_dois);
+		
+		System.out.println(objeto_conta_um.atributoSaldoConta);
+		System.out.println(objeto_conta_dois.atributoSaldoConta);
 	}
 
 }
